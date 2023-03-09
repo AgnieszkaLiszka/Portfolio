8f912938cebf439df688287f0763b5ec220c3988
AgnieszkaLiszka <agnieszka.liszka@coderscrew.pl>
2023-03-09 12:35:18 +0100

    Change responsive and burger menu

--- scroll.js
+++ scroll.js
@@ -3,10 +3,10 @@ const logo = document.getElementById('header__main');
 function scrollPage(event) {
 
    if (window.scrollY > 0) {
-        document.getElementById('header__main').classList.add('header__main--scroll')
+        logo.classList.add('header__main--scroll')
         return 
     }
-    document.getElementById('header__main').classList.remove('header__main--scroll')   
+    logo.classList.remove('header__main--scroll')   
 }
 
 
