55b32ce207f2453f2452ee18086584ceb7f9f01c
AgnieszkaLiszka <agnieszka.liszka@coderscrew.pl>
2023-04-05 15:34:55 +0200

    Make a modal box for contact section

---
+++ contact.js
@@ -0,0 +1,20 @@
+const modal = document.getElementById('contact__modal');
+const btn = document.getElementById('menu__item--modal');
+const x = document.getElementById('contact__close');
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = 'flex'
+}
+
+// When the user clicks on close button, close the modal
+x.onclick = function() {
+    modal.style.display = 'none'
+}
+
+//When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = 'none'
+    }
+}
