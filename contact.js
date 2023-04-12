99544d8d37341cb16df1f86231fa5990ee38025c
AgnieszkaLiszka <agnieszka.liszka@coderscrew.pl>
2023-04-12 10:49:57 +0200

    Rebuild Projects section and add Esc funcionality to Contact section

--- contact.js
+++ contact.js
@@ -12,9 +12,16 @@ x.onclick = function() {
     modal.style.display = 'none'
 }
 
-//When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target === modal) {
         modal.style.display = 'none'
     }
 }
+
+// When the user press Esc
+window.addEventListener ('keydown', function (event) {
+    if (event.key === 'Escape') {
+      modal.style.display = 'none'
+    }
+  })
